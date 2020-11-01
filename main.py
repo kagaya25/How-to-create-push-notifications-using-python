9be9f4816790d7d5cffd9ede410c07194a06f4e9
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-11-01 12:23:44 +0900

    done
    done

---
+++ main.py
@@ -0,0 +1,8 @@
+from plyer import notification 
+notification.notify(
+title="Pewdiepie new video",
+app_name ="youtubenotification",
+app_icon="C:/Users/kagaya/Downloads/1.ico", #put your ico path here
+message="You can write any message here",
+timeout=5
+)
